90cd964c594269dfb8500d45ebfa6d1e79e443c5
son588 <youaretehbestson588@gmail.com>
2015-02-12 13:09:29 -0500

    Updated this. Still need to test/see if their is errors

--- src/me/block3/Stats/Events/onPlayerDeath.java
+++ src/me/block3/Stats/Events/onPlayerDeath.java
@@ -4,4 +4,19 @@ package me.block3.Stats.Events;
  * Created by Dominic on 1/25/2015.
  */
 public class onPlayerDeath {
+  Base base = new Base();
+  
+  @EventHandler(priority = EventPriority.HIGHEST)
+  public void onPlayerDeath(PlayerDeathEvent e){
+    UUID uuid = e.getPlayer().getUniqueId();
+    String name = uuid.toString();
+    
+    if(base.getPlayersFile().contains(name)){
+      int deathcount = base.getPlayersFile().getInt(name + ".deaths");
+      /*deathcount++;*/
+      base.getPlayersFile().set(name + ".deaths", deathcount++);
+      base.savePlayersFile();
+      base.reloadPlayersConfig();
+    }
+  }
 }
